c19a9fdd51744dd7eef3f1f51ed4e669978e6d46
atharvamulam <atharvamulam@gmail.com>
2024-02-20 23:46:51 +0530

    Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,31 +6,19 @@ import { BrowserRouter as Router,Routes, Switch, Route, BrowserRouter } from 're
 import Homepage from './components/Homepage';
 import FormPage from './components/FormPage';
 import QRPage from './components/QRPage';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
     <div className="h-screen bg-gradient-to-r from-indigo-200 to-teal-50">
-      {/* <Navbar />
-      <h1 className="text-3xl font-bold text-center">
-        Welcome to Vaxchain
-      </h1> */}
-      {/* <BrowserRouter> 
-      <Routes>
-        <Route exact path="/" component={Navbar} />
-        <Route path="/form" component={FormComponent} />
-      </Routes>
-      </BrowserRouter> */}
-   
       <BrowserRouter> 
         <Routes>
           <Route path="/" element={<Homepage/>} />
           <Route path="/form" element={<FormPage/>} />
           <Route path="/scanqr" element={<QRPage/>} />
+          <Route path="/about" element={<AboutPage/>} />
         </Routes>
       </BrowserRouter>
-      
-      
-     
     </div>
   );
 }
