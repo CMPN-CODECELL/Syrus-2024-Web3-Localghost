547d01018fd84abab5482f6a375bda942d9a565d
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2024-02-21 11:54:39 +0530

    integration

--- frontend/src/components/FormComponent.jsx
+++ frontend/src/components/FormComponent.jsx
@@ -1,19 +1,30 @@
+import React, { useState } from "react";
+import Web3 from "web3";
 
-import React, { useState } from 'react';
+let web3;
+
+if (window.ethereum) {
+  web3 = new Web3(window.ethereum);
+} else {
+  // Handle the case where the user doesn't have MetaMask or any other Ethereum-compatible browser extension
+  console.log(
+    "Non-Ethereum browser detected. You should consider installing MetaMask."
+  );
+}
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
-    vaccineName: '',
-    manufacturerName: '',
-    vaccineType: '',
-    dateOfExpiry: '',
-    batchNumber: ''
+    vaccineName: "",
+    manufacturerName: "",
+    vaccineType: "",
+    dateOfExpiry: "",
+    batchNumber: "",
   });
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -21,13 +32,494 @@ const FormComponent = () => {
     e.preventDefault();
     const formDataJson = JSON.stringify(formData);
     console.log(formDataJson);
+    (async () => {
+      if (window.ethereum) {
+        try {
+          await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          window.web3 = new Web3(window.ethereum);
+
+          var accounts = await web3.eth.getAccounts();
+          account = accounts[0];
+          document.getElementById("wallet-address").innerText = account;
+        } catch (error) {
+          if (error.code === 4001) {
+            // User rejected request
+            console.log("User rejected the request");
+          } else {
+            console.error(error);
+          }
+        }
+      }
+
+      contract = new web3.eth.Contract(ABI, ADDRESS);
+      document.getElementById("mint").onclick = () => {
+        contract.methods
+          .safeMint(account)
+          .send({ from: account, value: "1000000000000000" });
+      };
+    })();
+    console.log("token created");
   };
+  var account = null;
+  var contract = null;
+  const ABI = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "additionalData",
+          type: "string",
+        },
+      ],
+      name: "appendMetadata",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "approved",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "operator",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "bool",
+          name: "approved",
+          type: "bool",
+        },
+      ],
+      name: "ApprovalForAll",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "approve",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "_address",
+          type: "address",
+        },
+        {
+          internalType: "enum VaccineTokenNew.Role",
+          name: "_role",
+          type: "uint8",
+        },
+      ],
+      name: "grantRole",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "previousOwner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "newOwner",
+          type: "address",
+        },
+      ],
+      name: "OwnershipTransferred",
+      type: "event",
+    },
+    {
+      inputs: [],
+      name: "renounceOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "string",
+          name: "_tokenURI",
+          type: "string",
+        },
+      ],
+      name: "safeMint",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+        {
+          internalType: "bytes",
+          name: "_data",
+          type: "bytes",
+        },
+      ],
+      name: "safeTransferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "operator",
+          type: "address",
+        },
+        {
+          internalType: "bool",
+          name: "approved",
+          type: "bool",
+        },
+      ],
+      name: "setApprovalForAll",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "from",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "transferFrom",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "newOwner",
+          type: "address",
+        },
+      ],
+      name: "transferOwnership",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+      ],
+      name: "balanceOf",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "getApproved",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "owner",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "operator",
+          type: "address",
+        },
+      ],
+      name: "isApprovedForAll",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "name",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "ownerOf",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "bytes4",
+          name: "interfaceId",
+          type: "bytes4",
+        },
+      ],
+      name: "supportsInterface",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "tokenURI",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  const ADDRESS = "0xd3f725c0cbc4D35C1d796A280a0D03EB16aB0D62";
 
   return (
     <div className="container mx-auto mt-8">
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="companyName">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="companyName"
+          >
             Company Name
           </label>
           <input
@@ -41,7 +533,10 @@ const FormComponent = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="diseaseName">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="diseaseName"
+          >
             Disease Name
           </label>
           <input
@@ -55,8 +550,11 @@ const FormComponent = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="vaccineType">
-            Vaccine 
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="vaccineType"
+          >
+            Vaccine
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -69,7 +567,10 @@ const FormComponent = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lifecycle">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="lifecycle"
+          >
             Lifecycle
           </label>
           <input
@@ -83,7 +584,10 @@ const FormComponent = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="dateOfMftg">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="dateOfMftg"
+          >
             Made on
           </label>
           <input
@@ -98,7 +602,10 @@ const FormComponent = () => {
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="dateOfExpiry">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="dateOfExpiry"
+          >
             Date of expiry
           </label>
           <input
@@ -111,7 +618,7 @@ const FormComponent = () => {
             onChange={handleChange}
           />
         </div>
-       
+
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Web3 from 'web3';
-import Navbar from './Navbar';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Web3 from "web3";
+import Navbar from "./Navbar";
 
 const Login = () => {
   const navigate = useNavigate();
   const [web3, setWeb3] = useState(null);
-  const [selectedRole, setSelectedRole] = useState('');
-  const [error, setError] = useState('');
+  const [selectedRole, setSelectedRole] = useState("");
+  const [error, setError] = useState("");
 
   const handleRoleLogin = (role) => {
     setSelectedRole(role);
@@ -15,98 +15,116 @@ const Login = () => {
   };
 
   const handleMetaMaskLogin = async () => {
-    console.log(selectedRole)
+    console.log(selectedRole);
     try {
       if (!window.ethereum) {
-        throw new Error('MetaMask extension not detected');
+        throw new Error("MetaMask extension not detected");
       }
 
       const web3Instance = new Web3(window.ethereum);
       setWeb3(web3Instance);
 
       // Request account access
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
       const walletAddress = accounts[0]; // Get the first account address
 
       // Perform wallet address validation
       if (!walletAddress) {
-        throw new Error('No wallet address found');
+        throw new Error("No wallet address found");
       }
 
-      const isValidAddress = await validateWalletAddress(selectedRole, walletAddress);
-      console.log(isValidAddress)
+      const isValidAddress = await validateWalletAddress(
+        selectedRole,
+        walletAddress
+      );
+      console.log(isValidAddress);
       if (isValidAddress) {
-        setError('');
-        console.log('Wallet address validation successful for role:', selectedRole);
+        setError("");
+        console.log(
+          "Wallet address validation successful for role:",
+          selectedRole
+        );
 
         // Redirect based on the selected role
-        if (selectedRole === 'admin') {
-          navigate('/');
+        if (selectedRole === "admin") {
+          navigate("/");
         } else {
-          navigate('/userdashboard');
+          navigate("/userdashboard");
         }
       } else {
-        setError('Invalid wallet address for the selected role');
+        setError("Invalid wallet address for the selected role");
       }
     } catch (error) {
-      console.error('MetaMask authentication failed:', error.message);
-      setError('MetaMask authentication failed');
+      console.error("MetaMask authentication failed:", error.message);
+      setError("MetaMask authentication failed");
     }
   };
 
   // Function to validate wallet address
   // Function to validate wallet address
-const validateWalletAddress = async (role, address) => {
-  // Define valid wallet addresses for each role
-  const validAddresses = {
-    admin: ['0x6a2e7fdfffcb45844c8482d4e828fb86ecd29fa7', /* Add more admin addresses as needed */],
-    user: ['0x469cB9016ee68AAA195416708562f6D3b2e09d0D', /* Add more user addresses as needed */],
-    // Add more roles and corresponding valid wallet addresses as needed
+  const validateWalletAddress = async (role, address) => {
+    // Define valid wallet addresses for each role
+    const validAddresses = {
+      admin: [
+        "0x6a2e7fdfffcb45844c8482d4e828fb86ecd29fa7" /* Add more admin addresses as needed */,
+      ],
+      user: [
+        "0x469cB9016ee68AAA195416708562f6D3b2e09d0D" /* Add more user addresses as needed */,
+      ],
+      // Add more roles and corresponding valid wallet addresses as needed
+    };
+    console.log("USer ka address " + address);
+    console.log("Array mein address " + validAddresses[role]);
+
+    if (role === "admin") {
+      if (
+        address === "0x6a2e7fdfffcb45844c8482d4e828fb86ecd29fa7" ||
+        address === "0x11fac33125adba1419be146191c84e202caa5407"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (role === "user") {
+      if (
+        address === "0xbdc2109abed65d2d2abd30c5882d2c854aa5a094" ||
+        address === "0xd51cd7068f8c27b3486b94ed77fdd35000b9e43c"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    // Check if the address is in the list of valid addresses for the selected role
+    console.log(validAddresses[role].includes());
+    return validAddresses[role].includes(); // Convert address to lowercase for case-insensitive comparison
   };
-  console.log("USer ka address " + address)
-  console.log("Array mein address " + validAddresses[role])
-
-  if(role === 'admin'){
-    if(address === '0x6a2e7fdfffcb45844c8482d4e828fb86ecd29fa7'){
-      return true;
-    }
-    else {
-      return false
-    }
-  }
-  else if(role === 'user'){
-    if(address === '0xbdc2109abed65d2d2abd30c5882d2c854aa5a094'){
-      return true;
-    }
-    else {
-      return false
-    }
-  }
- 
-  // Check if the address is in the list of valid addresses for the selected role
-  console.log(validAddresses[role].includes())
-  return validAddresses[role].includes(); // Convert address to lowercase for case-insensitive comparison
-};
-
 
   return (
-    <div className='flex items-center  h-screen flex-col'>
+    <div className="flex items-center  h-screen flex-col">
       <Navbar />
       <div className="flex items-center h-screen   justify-centerbg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-            {error && <p className="mt-2 text-center text-sm text-red-600">{error}</p>}
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+            {error && (
+              <p className="mt-2 text-center text-sm text-red-600">{error}</p>
+            )}
           </div>
           <div className="flex justify-center w-96 space-x-4">
             <button
-              onClick={() => setSelectedRole('admin')}
+              onClick={() => setSelectedRole("admin")}
               className="w-1/2 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Login as Manufacturer
             </button>
             <button
-              onClick={() => setSelectedRole('user')}
+              onClick={() => setSelectedRole("user")}
               className="w-1/2 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
             >
               Login as User
@@ -130,5 +148,3 @@ const validateWalletAddress = async (role, address) => {
 };
 
 export default Login;
-
-
